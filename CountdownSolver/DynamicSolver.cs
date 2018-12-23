9959dd7606e393652143e243c50effe35bed5c62
Sarah Laflamme <sarah.laflamme@outlook.com>
2018-12-23 15:57:39 -0500

    Works on my machine

--- CountdownSolver/DynamicSolver.cs
+++ CountdownSolver/DynamicSolver.cs
@@ -24,84 +24,76 @@ namespace CountdownSolver
 
             for (int i = 2; i < numbers.Length + 1; ++i)
             {
-                int maximumSizeOfLeftOperand = i / 2;
+                CombinationCollection currentCombinations = new CombinationCollection(i, numbers);
 
-                for (int j = maximumSizeOfLeftOperand; j > 0; j--)
+                foreach (Combination combination in currentCombinations)
                 {
-                    int sizeOfLeftOperand = j;
-                    int sizeOfRightOperand = i - j;
+                    var possibleOperandsList = SplitCombinationIntoOperandsList(combination);
+                    possibleResultsFromCombination.Add(combination.ToString(), new Dictionary<int, List<string>>());
 
-                    CombinationCollection leftOperands = new CombinationCollection(sizeOfLeftOperand, numbers);
-                    foreach (Combination leftOperand in leftOperands)
+                    foreach (var operands in possibleOperandsList)
                     {
-                        var rightOperandNumbersSet = numbers.Except(leftOperand.OrderedNumbers).ToArray();
-                        CombinationCollection rightOperands = new CombinationCollection(sizeOfRightOperand, rightOperandNumbersSet);
-                        foreach (Combination rightOperand in rightOperands)
+                        var leftOperand = operands.Item1;
+                        var rightOperand = operands.Item2;
+                        var possibleResultsOfLeftOperand = possibleResultsFromCombination[leftOperand.ToString()].ToList();
+                        var possibleResultsOfRightOperand = possibleResultsFromCombination[rightOperand.ToString()].ToList();
+
+                        foreach (var leftResult in possibleResultsOfLeftOperand)
                         {
-                            var mergedCombination = new Combination(leftOperand.OrderedNumbers.Union(rightOperand.OrderedNumbers).ToArray(), numbers);
-                            possibleResultsFromCombination.Add(mergedCombination.ToString(), new Dictionary<int, List<string>>());
-
-                            var possibleResultsOfLeftOperand = possibleResultsFromCombination[leftOperand.ToString()].ToList();
-                            var possibleResultsOfRightOperand = possibleResultsFromCombination[rightOperand.ToString()].ToList();
-
-                            foreach (var leftResult in possibleResultsOfLeftOperand)
+                            int leftValue = leftResult.Key;
+                            string leftExpression = leftResult.Value[0];
+                            foreach (var rightResult in possibleResultsOfRightOperand)
                             {
-                                int leftValue = leftResult.Key;
-                                string leftExpression = leftResult.Value[0];
-                                foreach(var rightResult in possibleResultsOfRightOperand)
+                                List<int> possibleResults = new List<int>() { closestResult };
+                                int rightValue = rightResult.Key;
+                                string rightExpression = rightResult.Value[0];
+
+                                int sum = leftValue + rightValue;
+                                AddResultSafely(possibleResultsFromCombination[combination.ToString()], sum, BuildExpression(leftExpression, rightExpression, "+"));
+                                possibleResults.Add(sum);
+
+                                int product = leftValue * rightValue;
+                                AddResultSafely(possibleResultsFromCombination[combination.ToString()], product, BuildExpression(leftExpression, rightExpression, "*"));
+                                possibleResults.Add(product);
+
+                                if (leftValue > rightValue)
                                 {
-                                    List<int> possibleResults = new List<int>() { closestResult };
-                                    int rightValue = rightResult.Key;
-                                    string rightExpression = rightResult.Value[0];
+                                    int difference = leftValue - rightValue;
+                                    AddResultSafely(possibleResultsFromCombination[combination.ToString()], difference, BuildExpression(leftExpression, rightExpression, "-"));
+                                    possibleResults.Add(difference);
+                                }
+                                else if (rightValue > leftValue)
+                                {
+                                    int difference = rightValue - leftValue;
+                                    AddResultSafely(possibleResultsFromCombination[combination.ToString()], difference, BuildExpression(rightExpression, leftExpression, "-"));
+                                    possibleResults.Add(difference);
+                                }
 
-                                    int sum = leftValue + rightValue;
-                                    AddResultSafely(possibleResultsFromCombination[mergedCombination.ToString()], sum, BuildExpression(leftExpression, rightExpression, "+"));
-                                    possibleResults.Add(sum);
+                                if (leftValue % rightValue == 0)
+                                {
+                                    int quotient = leftValue / rightValue;
+                                    AddResultSafely(possibleResultsFromCombination[combination.ToString()], quotient, BuildExpression(leftExpression, rightExpression, "/"));
+                                    possibleResults.Add(quotient);
+                                }
+                                else if (rightValue % leftValue == 0)
+                                {
+                                    int quotient = rightValue / leftValue;
+                                    AddResultSafely(possibleResultsFromCombination[combination.ToString()], quotient, BuildExpression(rightExpression, leftExpression, "/"));
+                                    possibleResults.Add(quotient);
+                                }
 
-                                    int product = leftValue * rightValue;
-                                    AddResultSafely(possibleResultsFromCombination[mergedCombination.ToString()], product, BuildExpression(leftExpression, rightExpression, "*"));
-                                    possibleResults.Add(product);
+                                int newClosestResult = FindValueClosestToTarget(possibleResults, target);
+                                if (newClosestResult != closestResult)
+                                {
+                                    closestResult = newClosestResult;
+                                    closestCombination = combination;
+                                }
 
-                                    if (leftValue > rightValue)
-                                    {
-                                        int difference = leftValue - rightValue;
-                                        AddResultSafely(possibleResultsFromCombination[mergedCombination.ToString()], difference, BuildExpression(leftExpression, rightExpression, "-"));
-                                        possibleResults.Add(difference);
-                                    }
-                                    else if (rightValue > leftValue)
-                                    {
-                                        int difference = rightValue - leftValue;
-                                        AddResultSafely(possibleResultsFromCombination[mergedCombination.ToString()], difference, BuildExpression(rightExpression, leftExpression, "-"));
-                                        possibleResults.Add(difference);
-                                    }
-
-                                    if (leftValue % rightValue == 0)
-                                    {
-                                        int quotient = leftValue / rightValue;
-                                        AddResultSafely(possibleResultsFromCombination[mergedCombination.ToString()], quotient, BuildExpression(leftExpression, rightExpression, "/"));
-                                        possibleResults.Add(quotient);
-                                    }
-                                    else if (rightValue % leftValue == 0)
-                                    {
-                                        int quotient = rightValue / leftValue;
-                                        AddResultSafely(possibleResultsFromCombination[mergedCombination.ToString()], quotient, BuildExpression(rightExpression, leftExpression, "/"));
-                                        possibleResults.Add(quotient);
-                                    }
-
-                                    int newClosestResult = FindValueClosestToTarget(possibleResults, target);
-                                    if (newClosestResult != closestResult)
-                                    {
-                                        closestResult = newClosestResult;
-                                        closestCombination = mergedCombination;
-                                    }
-
-                                    if (closestResult == target)
-                                    {
-                                        return Tuple.Create(target, possibleResultsFromCombination[closestCombination.ToString()][target][0]);
-                                    }
+                                if (closestResult == target)
+                                {
+                                    return Tuple.Create(target, possibleResultsFromCombination[closestCombination.ToString()][target][0]);
                                 }
                             }
-
                         }
                     }
 
@@ -129,7 +121,7 @@ namespace CountdownSolver
         {
             int closestValueToTarget = int.MinValue;
 
-            foreach(int value in values)
+            foreach (int value in values)
             {
                 int differenceBetweenClosestAndTarget = Math.Abs(target - closestValueToTarget);
                 int differenceBetweenCurrentAndTarget = Math.Abs(target - value);
@@ -141,6 +133,47 @@ namespace CountdownSolver
 
             return closestValueToTarget;
         }
-        
+
+        private List<Tuple<Combination, Combination>> SplitCombinationIntoOperandsList(Combination combination)
+        {
+            var partialOperands = new List<Tuple<List<int>, List<int>>>();
+            partialOperands.Add(Tuple.Create(new List<int> { combination.OrderedNumbers[0] }, new List<int>()));
+
+            return SplitNumbersIntoBuckets(partialOperands, combination.OrderedNumbers.Skip(1).ToList());
+        }
+
+        private List<Tuple<Combination, Combination>> SplitNumbersIntoBuckets(List<Tuple<List<int>, List<int>>> partialOperandsList, List<int> remainingNumbers)
+        {
+            if (remainingNumbers.Count == 0)
+            {
+                return partialOperandsList
+                    .Where(partialOperands => partialOperands.Item1.Count > 0 && partialOperands.Item2.Count > 0)
+                    .Select(partialOperands =>
+                        Tuple.Create(
+                            new Combination(partialOperands.Item1.Count, partialOperands.Item1.ToArray()),
+                            new Combination(partialOperands.Item2.Count, partialOperands.Item2.ToArray())
+                        ))
+                    .Distinct()
+                    .ToList();
+            }
+            else
+            {
+                int number = remainingNumbers[0];
+                var newPartialOperandsList = partialOperandsList.SelectMany(partialOperands =>
+                    new List<Tuple<List<int>, List<int>>> {
+                        Tuple.Create(
+                            partialOperands.Item1.Concat(new List<int>() { number }).ToList(),
+                            partialOperands.Item2
+                        ),
+                        Tuple.Create(
+                            partialOperands.Item1,
+                            partialOperands.Item2.Concat(new List<int>() { number }).ToList()
+                        )
+                    }
+                ).ToList();
+                return SplitNumbersIntoBuckets(newPartialOperandsList, remainingNumbers.Skip(1).ToList());
+            }
+        }
+
     }
 }
